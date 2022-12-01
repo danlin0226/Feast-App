0166067c9cef683c98911a27bdc12dd241466dc8
danlin0226 <danlin0226@gmail.com>
2022-11-30 21:45:36 -0800

    create folder structure

---
+++ server/migrations/20221201052459_create_users_table.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.uuid("id").primary();
+    table.string("email").notNullable();
+    table.string("name").notNullable();
+    table.string("age");
+    table.string("gender");
+    table.string("city");
+    table.string("prompt1");
+    table.string("prompt2");
+    table.string("prompt3");
+    table.string("fb");
+    table.string("ig");
+    table.string("tt");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
